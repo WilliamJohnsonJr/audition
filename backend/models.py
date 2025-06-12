f9cb1b631c3927414565f722deee8119fb3773bc
Will Johnson <19559137+WilliamJohnsonJr@users.noreply.github.com>
2025-06-12 12:24:30 -0400

    init alembic, init db models

--- backend/models.py
+++ backend/models.py
@@ -1,44 +1,66 @@
 import os
-from sqlalchemy import Column, String, create_engine
+import re
+import enum
+from flask import Flask
+from sqlalchemy.orm import DeclarativeBase
+from datetime import date
+from sqlalchemy import Column, Enum, Integer, String, Date
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import json
 
-database_path = os.environ['DATABASE_URL']
+
+class Base(DeclarativeBase):
+    pass
+
+
+database_path = os.environ["DATABASE_URL"]
 if database_path.startswith("postgres://"):
-  database_path = database_path.replace("postgres://", "postgresql://", 1)
+    database_path = database_path.replace("postgres://", "postgresql://", 1)
 
-db = SQLAlchemy()
-
-'''
+db = SQLAlchemy(model_class=Base)
+migrate = Migrate()
+"""
 setup_db(app)
     binds a flask application and a SQLAlchemy service
-'''
-def setup_db(app, database_path=database_path):
+"""
+
+
+def setup_db(app: Flask, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    with app.app_context():
-      db.app = app
-      db.init_app(app)
-      db.create_all()
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return app
 
 
-'''
-Person
-Have title and release year
-'''
-class Person(db.Model):  
-  __tablename__ = 'People'
+class Gender(enum.Enum):
+    MALE = "M"
+    FEMALE = "F"
 
-  id = Column(db.Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+class Movie(db.Model):
+    __tablename__ = "movie"
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    id = Column(db.Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(Date)
+
+    def __init__(self, title: str, release_date: date):
+        if len(title) < 1:
+            raise ValueError("Invalid title")
+
+        self.title = title
+        self.release_date = release_date
+
+    def format(self):
+        return {"id": self.id, "title": self.title, "release_date": self.release_date}
+
+
+class Actor(db.Model):
+    __tablename__ = "actor"
+
+    id = Column(db.Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(Enum(Gender))
